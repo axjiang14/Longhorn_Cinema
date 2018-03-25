7c1e74b0f6ca4411b43470408d0d36a6ac5ee50f
Alina <alinad2015@gmail.com>
2018-03-25 18:46:17 -0500

    DAL

---
+++ AWO_Team14/AWO_Team14/DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AWO_Team14.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyDBConnection") { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
