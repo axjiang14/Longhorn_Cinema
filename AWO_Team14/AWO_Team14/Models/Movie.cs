3a0004a8706977f5cf3a2d95a8ceab8d28d6627e
unknown <axjiang14@gmail.com>
2018-03-25 18:41:19 -0500

    Movie and Genre Classes

---
+++ AWO_Team14/AWO_Team14/Models/Movie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AWO_Team14.Models
+{
+    public enum MPAA { G, PG, PG13, R, Unrated}
+
+    public class Movie
+    {
+        //START AT 3000
+        [Display(Name = "Movie ID")]
+        public Int32 MovieID { get; set; }
+
+        [Required(ErrorMessage ="Title is required")]
+        [Display(Name = "Title")]
+        public String Title { get; set; }
+
+        [Required(ErrorMessage = "Tagline is required")]
+        [Display(Name = "Tagline")]
+        public String Tagline { get; set; }
+
+        [Required(ErrorMessage = "Overview is required")]
+        [Display(Name = "Overview")]
+        public String Overview { get; set; }
+
+        [Required(ErrorMessage = "Release Year is required")]
+        [Display(Name = "Release Year")]
+        public DateTime ReleaseYear { get; set; }
+
+        [Required(ErrorMessage = "MPAA Rating is required")]
+        [Display(Name = "MPAA Rating")]
+        public MPAA MPAA_Rating { get; set; }
+
+        [Required(ErrorMessage = "Runtime is required")]
+        [Display(Name = "Run Time")]
+        public DateTime Runtime { get; set; }
+
+        [Required(ErrorMessage = "Actors are required")]
+        public String Actors { get; set; }
+
+        public virtual List<Genre> Genres { get; set; } 
+
+    }
+}
