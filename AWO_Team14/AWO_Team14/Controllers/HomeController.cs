dea608dac75288fcf26a7437c85ea8530591d923
Alina <alinad2015@gmail.com>
2018-03-25 22:19:17 -0500

    Home View

---
+++ AWO_Team14/AWO_Team14/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AWO_Team14.Models;
+using AWO_Team14.DAL;
+using System.Net;
+
+
+namespace AWO_Team14.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+    }
+
+    
+}
