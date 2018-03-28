a4b12b073883e4ecb7598e38dedc94317c1e936c
Rowen <rowengl@utexas.edu>
2018-03-28 18:49:10 -0500

    New models

---
+++ AWO_Team14/AWO_Team14/Models/Discount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AWO_Team14.Models
+{
+	public class Discount
+	{
+		public Int32 DiscountID { get; set; } 
+
+		[Display(Name = "Discount Name")]
+		public String DiscountName { get; set; }
+
+		[Display(Name = "Discount Value")]
+		public Decimal DiscountValue { get; set; }
+	}
+}
---
+++ AWO_Team14/AWO_Team14/Models/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AWO_Team14.Models
+{
+	public class Schedule
+	{
+		public Int32 ScheduleID { get; set; } 
+
+		[Display(Name = "Published")]
+		public Boolean Published { get; set; }
+
+		public virtual List<Showing> Showings { get; set; }
+	}
+}
