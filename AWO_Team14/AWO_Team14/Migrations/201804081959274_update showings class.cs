9e0847a38353c86ac0f3c11460f55515e7cad9b7
unknown <axjiang14@gmail.com>
2018-04-08 16:25:21 -0500

    scheduling validation pt 1

---
+++ AWO_Team14/AWO_Team14/Migrations/201804081959274_update showings class.cs
@@ -0,0 +1,26 @@
+namespace AWO_Team14.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateshowingsclass : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Showings", "StartTime", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Showings", "EndTime", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Movies", "RunTime");
+            AddColumn("dbo.Movies", "Runtime", c => c.Time(nullable: false, precision: 7));
+            DropColumn("dbo.Showings", "ShowTime");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Showings", "ShowTime", c => c.DateTime(nullable: false));
+            DropColumn("dbo.Movies", "RunTime");
+            AddColumn("dbo.Movies", "Runtime", c => c.Int(nullable: false));
+            DropColumn("dbo.Showings", "EndTime");
+            DropColumn("dbo.Showings", "StartTime");
+        }
+    }
+}
---
+++ AWO_Team14/AWO_Team14/Utilities/ScheduleValidation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AWO_Team14.Models;
+using AWO_Team14.DAL;
+
+namespace AWO_Team14.Utilities
+{
+    public static class ScheduleValidation
+    {
+        public static Boolean ShowingValidation(Showing showing)
+        {
+            AppDbContext db = new AppDbContext();
+
+            //check that endtime < 12:00:00 AM
+            if(showing.EndTime > showing.ShowDate.AddDays(1).AddSeconds(-1))
+            {
+                return false;
+            }
+
+            //check for overlap
+            var overlapQuery = from s in db.Showings
+                        select s;
+            overlapQuery = overlapQuery.Where(s => s.Theater == showing.Theater);
+            overlapQuery = overlapQuery.Where(s => (s.EndTime >= showing.StartTime && s.StartTime <= showing.StartTime) || (s.EndTime >= showing.EndTime && s.StartTime <= showing.EndTime));
+            if(overlapQuery.Count() >0)
+            {
+                return false;
+            }
+
+            //check for duplicate showing in other theatre
+            var query = from s in db.Showings
+                        select s;
+            if (showing.Theater == Theater.One)
+            {
+                query = query.Where(s => s.Theater == Theater.Two);
+            }
+            else
+            {
+                query = query.Where(s => s.Theater == Theater.One);
+            }
+                
+            query = query.Where(s => s.StartTime == showing.StartTime);
+            if(query.Count() > 0)
+            {
+                return false;
+            }
+
+            //showing is ok!
+            return true;
+        }
+        
+
+    }
+}
