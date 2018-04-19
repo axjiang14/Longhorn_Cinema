ce0c2f0d75ea727680180a9b529332bdb3b98957
alexardenc <alexardenc@utexas.edu>
2018-04-18 20:15:51 -0500

    update Discounts

---
+++ AWO_Team14/AWO_Team14/Migrations/201804190110545_updateDiscount.cs
@@ -0,0 +1,16 @@
+namespace AWO_Team14.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateDiscount : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
