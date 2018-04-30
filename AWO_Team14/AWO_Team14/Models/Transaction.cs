800d7090624213fe75dabf77535f89b646304415
Rowen <rowengl@utexas.edu>
2018-04-29 23:50:03 -0500

    update transaction model

--- AWO_Team14/AWO_Team14/Models/Transaction.cs
+++ AWO_Team14/AWO_Team14/Models/Transaction.cs
@@ -7,13 +7,13 @@ using System.ComponentModel.DataAnnotations;
 namespace AWO_Team14.Models
 {
 	public enum Payment { PopcornPoints, CreditCard }
-    
 
-    public class Transaction
+
+	public class Transaction
 	{
-        private const Decimal SalesTaxRate = 0.0825m;
+		private const Decimal SalesTaxRate = 0.0825m;
 
-        public Int32 TransactionID { get; set; }
+		public Int32 TransactionID { get; set; }
 
 		[Display(Name = "Payment")]
 		public Payment Payment { get; set; }
@@ -21,34 +21,35 @@ namespace AWO_Team14.Models
 		[Display(Name = "Transaction Date")]
 		public DateTime TransactionDate { get; set; }
 
-        public Decimal Subtotal
-        {
-            get { return UserTickets.Sum(ut => ut.CurrentPrice); }
-        }
+		public Decimal Subtotal
+		{
+			get { return UserTickets.Sum(ut => ut.CurrentPrice); }
+		}
 
-        [Display(Name = "Sales Tax")]
-        public Decimal SalesTax
-        {
-            get { return Subtotal * SalesTaxRate; }
-        }
+		[Display(Name = "Sales Tax")]
+		public Decimal SalesTax
+		{
+			get { return Subtotal * SalesTaxRate; }
+		}
 
-        [Display(Name = "Order Total")]
-        [DisplayFormat(DataFormatString = "{0:c}")]
-        public Decimal Total
-        {
-            get { return Subtotal + SalesTax; }
-        }
+		[Display(Name = "Order Total")]
+		[DisplayFormat(DataFormatString = "{0:c}")]
+		public Decimal Total
+		{
+			get { return Subtotal + SalesTax; }
+		}
 
-        public virtual AppUser User { get; set; }
+		public virtual AppUser User { get; set; }
+		public virtual AppUser Giftee { get; set; }
 		public virtual List<UserTicket> UserTickets { get; set; }
 
-        public Transaction()
-        {
-            if (UserTickets == null)
-            {
-                UserTickets = new List<UserTicket>();
-            }
-        }
+		public Transaction()
+		{
+			if (UserTickets == null)
+			{
+				UserTickets = new List<UserTicket>();
+			}
+		}
 
-        }
+	}
 }
