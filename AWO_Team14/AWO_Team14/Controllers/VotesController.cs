0a9a7cda6e5b8e213662cf58a8a7aea0c6ceffd2
unknown <axjiang14@gmail.com>
2018-04-30 15:56:07 -0500

    votesssss

--- AWO_Team14/AWO_Team14/Controllers/VotesController.cs
+++ AWO_Team14/AWO_Team14/Controllers/VotesController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AWO_Team14.DAL;
 using AWO_Team14.Models;
+using Microsoft.AspNet.Identity;
 
 namespace AWO_Team14.Controllers
 {
@@ -15,11 +17,11 @@ namespace AWO_Team14.Controllers
     {
         private AppDbContext db = new AppDbContext();
 
-        // GET: Votes
-        public ActionResult Index()
-        {
-            return View(db.Votes.ToList());
-        }
+        //// GET: Votes
+        //public ActionResult Index()
+        //{
+        //    return View(db.Votes.ToList());
+        //}
 
         // GET: Votes/Details/5
         public ActionResult Details(int? id)
@@ -36,29 +38,46 @@ namespace AWO_Team14.Controllers
             return View(vote);
         }
 
+        [Authorize]
         // GET: Votes/Create
-        public ActionResult Create()
+        public ActionResult Create(Int32 id)
         {
-            return View();
+            Vote vote = new Vote();
+
+            MovieReview mr = db.MovieReviews.Find(id);
+
+            vote.MovieReview = mr;
+
+            return View(vote);
         }
 
         // POST: Votes/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "VoteID,ThumbVote")] Vote vote)
+        public ActionResult Create(Vote vote)
         {
+            MovieReview mr = db.MovieReviews.Find(vote.MovieReview.MovieReviewID);
+            vote.MovieReview = mr;
+
+            String UserID = User.Identity.GetUserId();
+            AppUser user = db.Users.Find(UserID);
+
+            vote.User = user;
+
             if (ModelState.IsValid)
             {
                 db.Votes.Add(vote);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Details", "Home", new {id = vote.MovieReview.Movie.MovieID });
             }
 
             return View(vote);
         }
 
+        [Authorize]
         // GET: Votes/Edit/5
         public ActionResult Edit(int? id)
         {
@@ -79,6 +98,7 @@ namespace AWO_Team14.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Edit([Bind(Include = "VoteID,ThumbVote")] Vote vote)
         {
             if (ModelState.IsValid)
@@ -90,31 +110,31 @@ namespace AWO_Team14.Controllers
             return View(vote);
         }
 
-        // GET: Votes/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Vote vote = db.Votes.Find(id);
-            if (vote == null)
-            {
-                return HttpNotFound();
-            }
-            return View(vote);
-        }
+        //// GET: Votes/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Vote vote = db.Votes.Find(id);
+        //    if (vote == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(vote);
+        //}
 
-        // POST: Votes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Vote vote = db.Votes.Find(id);
-            db.Votes.Remove(vote);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Votes/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Vote vote = db.Votes.Find(id);
+        //    db.Votes.Remove(vote);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
